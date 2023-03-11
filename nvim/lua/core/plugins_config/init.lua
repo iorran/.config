8e0b0b86ac032452af1bcbae646cebf31efe1114
Iorran Castro <iorranpt@gmail.com>
2023-03-11 01:36:06 +0000

    Init vim config

---
+++ nvim/lua/core/plugins_config/init.lua
@@ -0,0 +1,5 @@
+require("core.plugins_config.gruvbox")
+require("core.plugins_config.lualine")
+require("core.plugins_config.nvim-tree")
+require("core.plugins_config.treesitter")
+require("core.plugins_config.telescope")
